0e539c847d2faaf18d520b6b46d5aa2f18bdb0e2
Michael Edwards <EDWARDSMR@ETSU.EDU>
2020-10-04 06:59:17 -0400

    Create bicycle form with OnSubmit call(function not implemented). View with debugger to see the inputted serialnumber being passed into the bicycle.

--- Models/Bicycle.cs
+++ Models/Bicycle.cs
@@ -1,34 +1,58 @@
 using System.Collections.Generic;
-
+using System.ComponentModel.DataAnnotations;
 namespace frontendapi_bikeshop.Models
 {
     public class Bicycle
     {
-
+        [Required]
         public int Serialnumber { get; set; }
+        [Required]
         public int? Customerid { get; set; }
+        [Required]
         public string Modeltype { get; set; }
+        [Required]
         public int? Paintid { get; set; }
+        [Required]
         public int? Framesize { get; set; }
+        [Required]
         public int? Shipemployee { get; set; }
+        [Required]
         public int? Frameassembler { get; set; }
+        [Required]
         public int? Painter { get; set; }
+        [Required]
         public string Construction { get; set; }
+        [Required]
         public int? Waterbottlebrazeons { get; set; }
+        [Required]
         public string Customname { get; set; }
+        [Required]
         public string Letterstyleid { get; set; }
+        [Required]
         public int? Storeid { get; set; }
+        [Required]
         public int? Employeeid { get; set; }
+        [Required]
         public int? Toptube { get; set; }
+        [Required]
         public int? Chainstay { get; set; }
+        [Required]
         public int? Headtubeangle { get; set; }
+        [Required]
         public int? Seattubeangle { get; set; }
+        [Required]
         public int? Listprice { get; set; }
+        [Required]
         public int? Saleprice { get; set; }
+        [Required]
         public int? Salestax { get; set; }
+        [Required]
         public string Salestate { get; set; }
+        [Required]
         public int? Shipprice { get; set; }
+        [Required]
         public int? Frameprice { get; set; }
+        [Required]
         public int? Componentlist { get; set; }
         public Paint Paint { get; set; }
     }
