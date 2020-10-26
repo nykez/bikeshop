c23d58765dcd794d371a358e67203982862db5eb
Jordan C <nykezgaming@live.com>
2020-10-26 13:43:12 -0400

    iinject our auth service

--- Program.cs
+++ Program.cs
@@ -1,15 +1,13 @@
 using System;
 using System.Net.Http;
-using System.Collections.Generic;
 using System.Threading.Tasks;
-using System.Text;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
 using Blazored.Toast;
 using CurrieTechnologies.Razor.SweetAlert2;
 using Microsoft.AspNetCore.Components.Authorization;
+using Blazored.LocalStorage;
+using frontendapi_bikeshop.Services;
 
 namespace frontendapi_bikeshop
 {
@@ -21,6 +19,10 @@ namespace frontendapi_bikeshop
             builder.RootComponents.Add<App>("app");
 
             builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("https://se2-database.azurewebsites.net") });
+            builder.Services.AddBlazoredLocalStorage();
+            builder.Services.AddAuthorizationCore();
+            builder.Services.AddScoped<AuthenticationStateProvider, ApiAuthenticatedStateProvider>();
+            builder.Services.AddScoped<IAuthService, AuthService>();
             builder.Services.AddSweetAlert2();
             builder.Services.AddBlazoredToast();
 
