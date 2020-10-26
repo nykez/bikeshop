a6368404b05082e66c8b50eae9f0658acc8408f9
Jordan C <nykezgaming@live.com>
2020-10-26 01:13:08 -0400

    Create ApiAuthenticatedStateProvider.cs

---
+++ ApiAuthenticatedStateProvider.cs
@@ -0,0 +1,105 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Security.Claims;
+using System.Text.Json;
+using System.Threading.Tasks;
+using Blazored.LocalStorage;
+using Microsoft.AspNetCore.Components.Authorization;
+
+namespace frontendapi_bikeshop
+{
+    public class ApiAuthenticatedStateProvider : AuthenticationStateProvider
+    {
+        private readonly HttpClient _httpClient;
+        private readonly ILocalStorageService _localStorage;
+        public ApiAuthenticatedStateProvider(HttpClient httpClient, ILocalStorageService localStorage)
+        {
+            _localStorage = localStorage;
+            _httpClient = httpClient;
+        }
+
+        public override async Task<AuthenticationState> GetAuthenticationState()
+        {
+            // look for JWT
+            var savedToken = await _localStorage.GetItemAsync<string>("authToken");
+
+            // user is not logged in / not jwt
+            if (string.IsNullOrWhiteSpace(savedToken))
+            {
+                return new AuthenticationState(new ClaimsPrincipal(new ClaimsIdentity()));
+            }
+
+            // use our jwt to send up requests
+            _httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("bearer", savedToken);
+
+            return new AuthenticationState(new ClaimsPrincipal(new ClaimsIdentity(ParseClaimsFromJwt(savedToken), "jwt")));
+        }
+
+        public void MarkUserAsAuthenticated(string email)
+        {
+            var authenticatedUser = new ClaimsPrincipal(new ClaimsIdentity(new[] { new Claim(ClaimTypes.Name, email) }, "apiauth"));
+            var authState = Task.FromResult(new AuthenticationState(authenticatedUser));
+            NotifyAuthenticationStateChanged(authState);
+        }
+
+        public void MarkUserLoggedOut()
+        {
+            // create a blank identity
+            var anonymousUser = new ClaimsPrincipal(new ClaimsIdentity());
+            // pass that to the state => empty state
+            var authState = Task.FromResult(new AuthenticationState(anonymousUser));
+            // alert the service we've changed states
+            NotifyAuthenticationStateChanged(authState);
+        }
+
+        private IEnumerable<Claim> ParseClaimsFromJwt(string jwt)
+        {
+            var claims = new List<Claim>();
+            var payload = jwt.Split(".")[1];
+            var jsonBytes = ParseBase64WithoutPadding(payload);
+            var keyValuePairs = JsonSerializer.Deserialize<Dictionary<string, object>>(jsonBytes);
+
+            keyValuePairs.TryGetValue(ClaimTypes.Role, out object roles);
+
+            if (roles != null)
+            {
+                // multiple roles
+                if (roles.ToString().Trim().StartsWith("["))
+                {
+                    // deserial the roles into string[]
+                    var parsedRoles = JsonSerializer.Deserialize<string[]>(roles.ToString());
+
+                    // parse each role claim into the list
+                    foreach(var parsedRole in parsedRoles)
+                    {
+                        claims.Add(new Claim(ClaimTypes.Role, parsedRole));
+                    }
+                }
+                else
+                {
+                    // just a single role
+                    claims.Add(new Claim(ClaimTypes.Role, roles.ToString()));
+                }
+            }
+
+            // add all the 'claims' from the JWT -> username, email,... etc
+            claims.AddRange(keyValuePairs.Select(kvp => new Claim(kvp.Key, kvp.Value.ToString())));
+
+            return claims;
+            
+        }
+
+        private byte[] ParseBase64WithoutPadding(string base64)
+        {
+            switch (base64.Length % 4)
+            {
+                case 2: base64 += "=="; break;
+                case 3: base64 += "="; break;
+            }
+            return Convert.FromBase64String(base64);
+        }
+    }
+}
